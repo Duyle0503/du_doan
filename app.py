2a9ff9c9268955060fd165e5ac9d5fbfd6e18b86
MyPC <lebaoduy0503@gmail.com>
2022-12-13 22:44:09 +0700

    v2

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ from wtforms.validators import DataRequired
 from flask import render_template, flash, redirect
 import pickle
 import numpy as np
-import os 
-import codecs
+ 
+# sql
+from sqlalchemy import create_engine    
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 # Mở các file model
 file = open('templates/runtime.txt', mode="r", encoding='utf-8')
@@ -20,7 +22,8 @@ key_1, key_2, key_3, key_4, key_5, key_6, key_7, key_21, key_8, key_9, key_10, k
 # Khai báo flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'zky_nah'
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql + pymysql://root:12345@localhost/model1"
+engine = create_engine("mysql+pymysql://root")
 def stroke_prediction(input_data):
     # Đổi thông tin nhập vào thành dạng Array
     input_data_as_numpy_array = np.asarray(input_data)
@@ -128,13 +131,27 @@ def myth():
             key_gender = 0
         elif data_gen_other == True:
             key_gender = 2 
-      
+        sum_g = 0   
+        for gender in (data_gen_male, data_gen_female, data_gen_other):
+            if gender == True:
+                sum_g += 1
+            if sum_g >= 2:
+                flash('Nhập quá 2 giới tính.')
+                break
+
+
         if data_marr_N == True:
-         
             key_marr = 0  
         elif data_marr_Y == True:
             key_marr = 1 
-     
+        key_m = 0    
+        for marrr in (data_marr_N, data_marr_Y):
+            if marrr == True:
+                key_m += 1
+            if key_m == 2:
+                flash('Nhập sai tình trạng hôn nhân.')
+                break
+             
         if data_gvt == True:
             key_job = int(0) 
              
@@ -147,11 +164,26 @@ def myth():
             key_job = int(3)
         elif data_Child == True:
             key_job = int(4)
-        
+        k_job = 0 
+        for job in (data_Child,data_self, data_gvt, data_Nev,data_priv):
+            if job == True:
+                 k_job += 1
+            if  k_job >= 2:
+                flash('Nhập dư nghề nghiệp.')
+                break
+
         if data_rural == True:
             key_live = int(0)  
         elif data_urban == True:
             key_live = int(1)
+
+        key_l = 0    
+        for live in (data_rural, data_urban):
+            if live == True:
+                key_l += 1
+            if key_l == 2:
+                flash('Nhập thừa nơi sống.')
+                break    
     # Tiểu sử hút thuốc
         if data_unk_sm == True:
             key_smoke = int(0)  
@@ -164,7 +196,13 @@ def myth():
              
         elif data_sta_sm == True:
             key_smoke = int(3) 
-
+        key_mk = 0    
+        for marrr in (data_unk_sm, data_use_sm, data_nev_sm, data_sta_sm):
+            if marrr == True:
+                key_mk += 1
+            if key_mk >= 2:
+                flash('Nhập dư tiền sử hút thuốc.')
+                break
         # trả về tham số    
         return  [key_gender, key_age, key_hyper, key_heart, key_marr, key_job, key_live, key_glucozo, key_bmi, key_smoke]
     if form.validate_on_submit():
@@ -173,20 +211,22 @@ def myth():
         prediction_proba, score_predict = stroke_prediction(setz)
         prediction, score_deep  = stroke_prediction_deep(setz)
         round(score_predict, 2)
+         
+       
+
         # in kết quả dự đoán
         if(score_predict == 0):
             data = "Nhấn nút để nhận dự đoán!"
-            return '''<div id="result"><h1>Kết quả: {}</h1> </div>'''.format(data)
         elif(score_predict >= 75 and score_deep == 0):
             data = f'Bạn có {round(score_predict, 2)} % khả năng bị Đột quỵ, tìm bác sĩ để nhận chuẩn đoán chi tiết.'
-            return '''<div id="result"><h1>Kết quả: {}</h1> </div>'''.format(data)
         elif(score_predict >= 40 and score_predict < 75 and score_deep == 0):
             data = f'Bạn có {round(score_predict, 2)} % khả năng bị Đột quỵ, nguy cơ này là rất cao.'
-            return '''<div id="result"><h1>Kết quả: {}</h1> </div>'''.format(data)
         else:
             data = f'Bạn có {round(score_predict, 2)} % khả năng bị Đột quỵ.'
-            return '''<div id="result"><h1>Kết quả: {}</h1> </div>'''.format(data)
-
+        flash(data)
+             
+           # return '''<div id="result"><h1>Kết quả: {}</h1> </div>'''.format(data)
+       
          
 
     return render_template('myth.html', the_title='Bắt đầu', form=form )
